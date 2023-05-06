f6a5d56ead0ef3190dda74d7d01b788728f7a73e
wangshuai <15877410869@163.com>
2023-05-06 17:26:49 +0800

    📖 EventEmitter

---
+++ docs/面试/EventEmitter.js
@@ -0,0 +1,74 @@
+class EventEmitter {
+  /**
+   * @constructor
+   */
+  constructor() {
+    this.eventList = [];
+  }
+
+  /**
+   * 监听事件
+   * @param {string} eventName
+   * @param {callback} callback
+   * @param {boolean | Object} [options = true]
+   * @param {Element | Document} [target = document]
+   */
+  $on(eventName, callback, options = true, target = document) {
+    target.addEventListener(eventName, callback, options);
+    this.eventList.push({
+      target,
+      eventName,
+      callback,
+      target,
+    })
+  }
+
+  /**
+   * 触发事件
+   * @param {string} eventName
+   * @param {any} detail
+   * @param {Element | Document} [target = document]
+   */
+  $emit(eventName, data, target = document) {
+    const customEvent = new CustomEvent(eventName, {
+      detail,
+      bubbles: true,
+    });
+    target.dispatchEvent(customEvent);
+  }
+
+  /**
+   * 注销事件
+   * @param {string} eventName
+   * @param {callback} callback
+   * @param {boolean | Object} [options = true]
+   * @param {Element | Document} [target = document]
+   */
+  $off(eventName, callback, options = true, target = document) {
+    let eventList = [];
+    if (eventName && callback && options && target) {
+      eventList = this.eventList.filter(
+        e => e.eventName === eventName && e.callback === callback && e.options === options && e.target === target
+      )
+    } else if (eventName && callback && options) {
+      eventList = this.eventList.filter(
+        e => e.eventName === eventName && e.callback === callback && e.options === options
+      )
+    } else if (eventName && callback) {
+      eventList = this.eventList.filter(
+        e => e.eventName === eventName && e.callback === callback
+      )
+    } else if (eventName) {
+      eventList = this.eventList.filter(
+        e => e.eventName === eventName
+      )
+    } else {
+      eventList = [...this.eventList];
+    }
+    eventList.forEach(e => {
+      e.target.removeEventListener(e.eventName, e.callback, e.options)
+      const index = this.eventList.findIndex(event => event === e);
+      if(~index) this.eventList.splice(index, 1)
+    })
+  }
+}
